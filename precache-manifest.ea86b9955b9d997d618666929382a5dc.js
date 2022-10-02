a843f5f4473d01e2fa869a510bc773626f8c66a7
JoshuaKiplimo <joshuakiprono8@gmail.com>
2022-10-02 15:37:12 -0400

    Updates

---
+++ precache-manifest.ea86b9955b9d997d618666929382a5dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90f9836957ec364d42665082b613e9a5",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "0231de5cbab1b72602ff",
+    "url": "/ToDoList/static/css/main.e6bb01d6.chunk.css"
+  },
+  {
+    "revision": "a5640d4bcbc7f4184abc",
+    "url": "/ToDoList/static/js/2.79ac4408.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoList/static/js/2.79ac4408.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0231de5cbab1b72602ff",
+    "url": "/ToDoList/static/js/main.c8f6e58c.chunk.js"
+  },
+  {
+    "revision": "054e3d803821fb883599",
+    "url": "/ToDoList/static/js/runtime-main.5aae5f9a.js"
+  }
+]);
